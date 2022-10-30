c967f1ac7d8cdbb0aab1f62fa013c5072d73659a
Nirajan1 <nirajanjoshi80@gmail.com>
2022-10-30 21:14:03 +0545

    listview

--- lib/utli/emoticon_rating.dart
+++ lib/utli/emoticon_rating.dart
@@ -2,24 +2,38 @@ import 'package:flutter/material.dart';
 
 class EmotIconRating extends StatelessWidget {
   final Icon emoticon;
+  final String title;
   const EmotIconRating({
     super.key,
     required this.emoticon,
+    required this.title,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: Colors.blue[400],
-        borderRadius: BorderRadius.circular(12),
-      ),
-      child: IconButton(
-        color: Colors.white,
-        iconSize: 20,
-        onPressed: () {},
-        icon: emoticon,
-      ),
+    return Column(
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            color: Colors.blue[400],
+            borderRadius: BorderRadius.circular(12),
+          ),
+          child: IconButton(
+            color: Colors.white,
+            iconSize: 20,
+            onPressed: () {},
+            icon: emoticon,
+          ),
+        ),
+        const SizedBox(
+          height: 5.0,
+        ),
+        Text(
+          title,
+          style: const TextStyle(
+              color: Colors.white, fontSize: 12, fontWeight: FontWeight.bold),
+        ),
+      ],
     );
   }
 }
--- lib/view/home.dart
+++ lib/view/home.dart
@@ -1,5 +1,6 @@
 import 'package:copyd/utli/emoticon_rating.dart';
 import 'package:flutter/material.dart';
+import 'package:get/route_manager.dart';
 import 'package:intl/intl.dart';
 
 class HomePage extends StatelessWidget {
@@ -28,10 +29,12 @@ class HomePage extends StatelessWidget {
         backgroundColor: const Color(0xff277ebe),
         body: SingleChildScrollView(
           child: Column(
+            //main column inside scroll area//
             children: [
               Padding(
                 padding: const EdgeInsets.all(20.0),
                 child: Column(
+                  //column inside padding //
                   children: [
                     // greating row
                     Row(
@@ -49,7 +52,8 @@ class HomePage extends StatelessWidget {
                               height: 2.0,
                             ),
                             Text(
-                              DateFormat('d - MMMM - y').format(time),
+                              DateFormat('MMMM - dd - y')
+                                  .format(time), //gives current date/time/year
                               style: const TextStyle(
                                 fontSize: 15,
                                 color: Colors.white,
@@ -69,12 +73,14 @@ class HomePage extends StatelessWidget {
                               icon: const Icon(Icons.notifications)),
                         )
                       ],
-                    ),
+                    ), //greating row ends here//
                     // search bar row
                     const SizedBox(
                       height: 20.0,
                     ),
+
                     Container(
+                      //static searchbar design //
                       decoration: BoxDecoration(
                         color: Colors.blue[400],
                         borderRadius: BorderRadius.circular(12),
@@ -82,6 +88,7 @@ class HomePage extends StatelessWidget {
                       child: Padding(
                         padding: const EdgeInsets.all(8.0),
                         child: Row(
+                          //searchbar to be changed more dynamic
                           children: const [
                             Icon(
                               Icons.search,
@@ -98,22 +105,26 @@ class HomePage extends StatelessWidget {
                         ),
                       ),
                     ),
+
                     const SizedBox(
                       height: 20.0,
                     ),
-
+                    //static rating bar design//
                     Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: const [
-                        Text(
+                      children: [
+                        const Text(
                           'How do you feel?',
                           style: TextStyle(
                             color: Colors.white,
                           ),
                         ),
-                        Icon(
-                          Icons.more_horiz,
-                          color: Colors.white,
+                        IconButton(
+                          onPressed: () {},
+                          icon: const Icon(
+                            Icons.more_horiz,
+                            color: Colors.white,
+                          ),
                         ),
                       ],
                     ),
@@ -122,76 +133,102 @@ class HomePage extends StatelessWidget {
                     ),
                     Row(
                       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        Column(
-                          children: const [
-                            EmotIconRating(
-                                emoticon: Icon(
-                              Icons.sentiment_very_satisfied,
-                              size: 20,
-                            )),
-                            SizedBox(
-                              height: 5.0,
-                            ),
-                            Text(
-                              'Excellent',
-                              style:
-                                  TextStyle(color: Colors.white, fontSize: 12),
-                            ),
-                          ],
+                      children: const [
+                        EmotIconRating(
+                          emoticon: Icon(
+                            Icons.sentiment_very_satisfied,
+                            size: 20,
+                          ),
+                          title: 'Excellent',
                         ),
-                        Column(
-                          children: const [
-                            EmotIconRating(
-                              emoticon: Icon(
-                                Icons.sentiment_satisfied,
-                                size: 20,
-                              ),
-                            ),
-                            SizedBox(height: 5.0),
-                            Text(
-                              'Fine',
-                              style:
-                                  TextStyle(color: Colors.white, fontSize: 12),
-                            )
-                          ],
+                        EmotIconRating(
+                          emoticon: Icon(
+                            Icons.sentiment_satisfied,
+                            size: 20,
+                          ),
+                          title: 'Fine',
                         ),
-                        Column(
-                          children: const [
-                            EmotIconRating(
-                                emoticon: Icon(
-                              Icons.sentiment_dissatisfied,
-                              size: 20,
-                            )),
-                            SizedBox(height: 5.0),
-                            Text(
-                              'Well',
-                              style:
-                                  TextStyle(color: Colors.white, fontSize: 12),
-                            )
-                          ],
+                        EmotIconRating(
+                          emoticon: Icon(
+                            Icons.sentiment_dissatisfied,
+                            size: 20,
+                          ),
+                          title: 'Well',
                         ),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: const [
-                            EmotIconRating(
-                                emoticon: Icon(
-                              Icons.sentiment_very_dissatisfied,
-                              size: 20,
-                            )),
-                            SizedBox(height: 5.0),
-                            Text(
-                              'Badly',
-                              style:
-                                  TextStyle(color: Colors.white, fontSize: 12),
-                            )
-                          ],
+                        EmotIconRating(
+                          emoticon: Icon(
+                            Icons.sentiment_very_dissatisfied,
+                            size: 20,
+                          ),
+                          title: 'Bad',
                         ),
                       ],
                     ),
+
+                    //static rating bar design ends here//
                   ],
                 ),
                 //  top container inside the padding is closed!
+              ),
+              // white container bellow the padding with edged curve
+              const SizedBox(
+                height: 20.0,
+              ),
+              Container(
+                width: Get.size.width,
+                height: 329,
+                decoration: BoxDecoration(
+                  color: Colors.grey.shade100,
+                  borderRadius: const BorderRadius.only(
+                    topLeft: Radius.circular(20.0),
+                    topRight: Radius.circular(20.0),
+                  ),
+                ),
+                child: Column(
+                  children: [
+                    const Center(
+                      child: Icon(Icons.linear_scale_sharp),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: const [
+                          Text(
+                            'Excercises',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 15),
+                          ),
+                          Icon(Icons.more_horiz)
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 25, right: 25),
+                      child: Container(
+                        height: 90,
+                        decoration: const BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(20.0),
+                          ),
+                        ),
+                        child: Center(
+                          child: ListTile(
+                            // leading: ,
+                            title: const Text("Speaking skils"),
+                            subtitle: const Text("16 Excercises"),
+                            leading: const Icon(Icons.favorite),
+                            trailing: IconButton(
+                              onPressed: () {},
+                              icon: const Icon(Icons.more_horiz),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ],
           ),
